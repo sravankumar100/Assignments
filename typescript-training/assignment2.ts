8370094ae89be619c9f020f46d20ff38d586326f
Sravankumar <badesravankumar1000@gmail.com>
2026-01-13 20:25:53 +0530

    Assignment2-Datatype

---
+++ typescript-training/assignment2.ts
@@ -0,0 +1,80 @@
+// 1. Temperature of a city in degrees Celsius: 25.5
+let temparature:number=25.5;
+console.log("Temperature of a city:" +temparature);
+
+// 2. Whether a customer has placed an order: true or false
+let orderPlaced:boolean=true;
+console.log("customer has placed an order:" +orderPlaced);
+
+// 3. Person's phone number: "123-456-7890"
+let number1:number=1234567890;
+console.log("Person's phone number:" +number1);
+
+// 4. Amount of money in a customer's bank account: 1000.50
+let money:number=1000.50;
+console.log("Amount of money in a customer's bank account:" +money);
+
+// 5. Person's email address: "john.doe@example.com"
+let address:String="john.doe@example.com";
+console.log("Person's email address:" +address);
+
+// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+const latitude:number = 37.7749; 
+const longitude:number = -122.4194;
+console.log("Coordinates of a location :" +latitude + " ,"+longitude);
+
+// 7. Person's marital status: true or false
+let maritalStatus:boolean=true;
+console.log("Person's marital status:" +maritalStatus);
+
+// 8. Person's occupation: "Software Engineer"`
+let occupation:String="Software Engineer";
+console.log("Person's occupation:" +occupation);
+
+// 9. Person's favourite colour: "Blue"
+let favouriteColour:String="Blue";
+console.log("Person's favourite colour:" +favouriteColour);
+
+// 10.Current year: 2023
+let year:number=2023;
+console.log("Currentyear:" +year);
+
+// 11.Number of followers on a social media platform: 1,000,000
+let socialmediaFollowers:number=1000000;
+console.log("Number of followers on a social media platform::" +socialmediaFollowers);
+
+// 12.Rating of a movie: 7.5
+let movieRating:number=7.5;
+console.log("movie rating is:" +movieRating);
+
+// 13.Person's blood type: 'A'
+const bloodType:String='A';
+console.log("Person's blood type:" +bloodType);
+
+// 14.Title of a book: "To Kill a Mockingbird"
+let bookTitle:String="To Kill a Mockingbird";
+console.log("Title of a book:" +bookTitle);
+
+// 15.Number of employees in a company: 500
+let employeeCount:number=500;
+console.log("Number of employees in a company:" +employeeCount);
+
+// 16.Time of an event: 2:30 PM
+let eventTime:String="2:30PM";
+console.log("Time of an event:" +eventTime);
+
+// 17.Name of a country: "United States"
+let country:String="United States";
+console.log("Name of a country:" +country);
+
+// 18.Person's eye color: "Brown"
+let eyeColor:String="Brown";
+console.log("Person's eye color:" +eyeColor);
+
+// 19.Person's birthplace: "New York City"
+let birthPlace:String="New York City";
+console.log("Person's birthplace:" +birthPlace);
+
+// 20. Distance between two cities: 200.5
+let distance:number=200.5;
+console.log("Distance between two cities:" +distance);   
