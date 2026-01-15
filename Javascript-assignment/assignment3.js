8fdee45c1b35d13016315cc72e3da605c119c7e8
Sravankumar <badesravankumar1000@gmail.com>
2026-01-15 10:20:48 +0530

    Updated

---
+++ Javascript-assignment/assignment3.js
@@ -0,0 +1,15 @@
+function loanEligibility(customerName,creditscore,income,isEmployed,DTIratio){
+if(creditscore >750){
+console.log("loan is automatically approved");
+console.log("loan is automatically approved for:" +customerName);
+}
+else if(creditscore >=650 &&creditscore <=700 && income >=50000 && isEmployed &&DTIratio <=40 ){
+    console.log("loan is automatically approved");
+    console.log("loan is automatically approved for:" +customerName);
+}
+else{
+    console.log("loan is denied");
+    console.log("loan is denied for:" +customerName);
+}
+}
+loanEligibility("John Doe",620,4 5000,true,35000);
---
+++ javascript-training/variables.js
@@ -0,0 +1,22 @@
+let a=10;//initialisation
+let b="Sravan";
+let c=true;
+b="Virat";
+//let and const will have block level scope
+//redeclaration is allowed for Var
+//reassignment is allowed for all
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+const e=3.14;
+console.log(e);
+let d;//declaration
+let f=null;
+console.log(f);
+
+
+
