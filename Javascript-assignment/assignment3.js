cda42d4d45669292487d8d1c9d463b5220d0e6e3
Sravankumar <badesravankumar1000@gmail.com>
2026-01-16 08:28:04 +0530

    updated

--- Javascript-assignment/assignment3.js
+++ Javascript-assignment/assignment3.js
@@ -1,15 +1,37 @@
-function loanEligibility(customerName,creditscore,income,isEmployed,DTIratio){
-if(creditscore >750){
-console.log("loan is automatically approved");
-console.log("loan is automatically approved for:" +customerName);
+function PersonEligibilityLoan(customerName, creditScore, income, isEmployed, debtToIncomeRatio) 
+{ 
+if(creditScore>750){
+    console.log("loan is approved automatically");
+     console.log("loan is approved automatically for:" +customerName);
+    }
+    else if(creditScore>=650 && creditScore<=750){
+        if(income >=50000){
+            if(isEmployed){
+
+                if(debtToIncomeRatio<40){
+                     console.log("loan is approved automatically");
+     console.log("loan is approved automatically for:" +customerName);
+                }
+                else{
+                     console.log("loan is denied ");
+     console.log("loan is denied because of DTI ratio for:" +customerName);
+                }
+            }
+                  else{
+                     console.log("loan is denied ");
+     console.log("loan is denied because User is Unemployed for:" +customerName);
+                }
+            }
+                else{
+                     console.log("loan is denied ");
+     console.log("loan is denied because User income is lesser than 55000 for:" +customerName);
+                }
+            }
+                 else{
+                     console.log("loan is denied ");
+     console.log("loan is denied because User creditscore less than 650 for:" +customerName);
+                }
+            
 }
-else if(creditscore >=650 &&creditscore <=700 && income >=50000 && isEmployed &&DTIratio <=40 ){
-    console.log("loan is automatically approved");
-    console.log("loan is automatically approved for:" +customerName);
-}
-else{
-    console.log("loan is denied");
-    console.log("loan is denied for:" +customerName);
-}
-}
-loanEligibility("John Doe",720,4 ,55000,true,35000);
+
+PersonEligibilityLoan("john Doe",720,55000,true,35);
